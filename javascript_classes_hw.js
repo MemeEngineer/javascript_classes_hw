6f0525e2c1eb53ca97155e117eee1850d0f44ca5
JWu <johnnyw131@gmail.com>
2023-07-10 14:21:53 -0400

    JavaScript Classes Hw completed

---
+++ javascript_classes_hw.js
@@ -0,0 +1,64 @@
+class Cat{
+    constructor(name, breed, cute) {
+        this.name = name;
+        this.breed = breed;
+        this.cute = cute;
+    }
+    cutie(){
+        this.cute = true
+        console.log('have a treat')
+    }
+    bark(){
+        console.log(`My name is ${this.name}`)
+    }
+    type(){
+        console.log(`I am a ${this.breed}`)
+    }
+}
+
+const milo = new Cat('milo', 'corgi', true)
+const garfield = new Cat( 'garfield', 'tabby', false)
+
+console.log(milo)
+console.log(garfield)
+
+milo.cutie()
+milo.bark()
+milo.type()
+garfield.cutie()
+garfield.bark()
+garfield.type()
+
+class Pirate{
+    constructor(name,ship,job){
+        this.name= name
+        this.ship = ship
+        this.job = job
+    }
+    attack(){
+        console.log('Prepare to board!')
+    }
+    catchphrase(){
+        console.log('I am going to be the Pirate King')
+    }
+    retreat(){
+        console.log("Back to the ship, matey!")
+    }
+}
+
+// const strawhat = [{name: 'luffy',ship: 'thousand sunny',job: 'captain'}, {name:'zorro',ship:'thousand sunny',job:'swordsman'},{name:'sanji',ship:'thousand sunny',job: 'chef'}]
+// const carribean = [{name:'sparrow',ship:'black pearl',job:'captain'},{name:'turner',ship:'black pearl',job:'first-mate'},{name:'swann',ship:'black pearl',job:'beauty'}]
+
+const p1 = new Pirate('luffy', 'thousand sunny', 'captain')
+const p2 = new Pirate('zoro', 'thousand sunny', 'swordsman')
+const p3 = new Pirate('sanji', 'thousand sunny', 'chef')
+
+const strawhat= [p1, p2, p3]
+const p4 = new Pirate('sparrow', 'blackpearl', 'captain')
+const p5 = new Pirate('turner', 'blackpearl', 'firstmate')
+const p6 = new Pirate('swann', 'blackpearl', 'beauty')
+const carribean= [p4,p5,p6]
+for(let i=0; i <= strawhat.length; i++){
+    console.log(strawhat[i])
+    console.log(carribean[i])
+}
